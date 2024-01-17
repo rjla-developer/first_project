0926c8f779417237f119e8f5efb634fd60354336
RJDalefon <rjluengas@dalefon.mx>
2024-01-17 09:06:48 -0600

    New Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:first_project/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 136, 48, 107),
+                Color.fromARGB(255, 121, 224, 219),
+                Colors.yellow
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
