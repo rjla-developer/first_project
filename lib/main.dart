2887a3a0cf2bbe1c3a132bfd85b13ddde901cace
RJDalefon <rjluengas@dalefon.mx>
2024-01-18 09:46:10 -0600

    Se crearon los archivo quiz.dart y questions_screen.dart Se agrego la variable activeScreen de tipo Widget para cambiar de pantalla más adelante

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:first_project/start_screen.dart';
+import 'package:first_project/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.yellow,
-                Color.fromARGB(255, 121, 224, 219),
-                Color.fromARGB(255, 136, 48, 107),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+import "package:first_project/start_screen.dart";
+import "package:first_project/questions_screen.dart";
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.yellow,
+                Color.fromARGB(255, 121, 224, 219),
+                Color.fromARGB(255, 136, 48, 107),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
