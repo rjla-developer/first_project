270f478ba39a7e01393ef56c6d4c8850b15d6619
RJDalefon <rjluengas@dalefon.mx>
2024-01-19 10:43:02 -0600

    Se coloco otra manera a la que se hizo en el archivo start_screen.dart para poder colocar en el centro el contenido. Se ocupo SizedBox, se le dio un ancho de double.infinity (Esta propiedad le dice que tenga el valor maximo posible de pantalla) y se ocupo mainAxisAligment el cual "determina cómo se deben alinear los hijos de un widget a lo largo del eje principal".

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("hey");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Respuesta 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Respuesta 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Respuesta 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
