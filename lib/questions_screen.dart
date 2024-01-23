aebace103e190eedcfa9199007a40174dcee7863
RJDalefon <rjluengas@dalefon.mx>
2024-01-23 16:54:13 -0600

    Se creo la lógica para escoger la respuesta que se está seleccionando en el quiz

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -8,7 +8,9 @@ import "package:first_project/data/questions.dart";
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectedAnswer});
+
+  final void Function(String answer) onSelectedAnswer;
 
   @override
   State<QuestionsScreen> createState() => _QuestionsScreenState();
@@ -17,7 +19,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectedAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -46,7 +49,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ...currentQuestion.getShuffledAnswers().map(
                   (answer) => AnswerButton(
                     answerText: answer,
-                    onTap: answerQuestion,
+                    onTap: () {
+                      answerQuestion(answer);
+                    },
                   ),
                 ),
           ],
