ab38dc980a0743bb6f9298c8577391e851535a52
RJDalefon <rjluengas@dalefon.mx>
2024-01-24 09:32:43 -0600

    Se creo una función que por medio de un Mapa(Diccionario en Visual Studio) pondremos los datos de: - Número de pregunta - Pregunta - Respueta correcta - Respuesta del usuario
    Se guardarán y los utilizaremos después

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_project/data/questions.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summaryData = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summaryData.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summaryData;
+  }
 
   @override
   Widget build(BuildContext context) {
